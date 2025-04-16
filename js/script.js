d91adf41cd7be3fd554f90cccc47abf5ae0005c5
LuanJsv <luan.o.dantas@gmail.com>
2025-04-15 22:17:19 -0300

    adicionando JS, mudando CSS, pequenos ajuste HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log('teste')
+const container = document.querySelector('.container')
+const qrcodebtn = document.querySelector('#qr-form button')
+const QRinput = document.querySelector('#qr-form input')
+const imgQR = document.querySelector('#qr-code img')
+// Eventos !
+// Gerando código QR
+function gerarQRcode(){
+    const QRinputValue =  QRinput.value
+    if(!QRinputValue) return;
+    qrcodebtn.innerText = "gerando código..."
+    imgQR.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${QRinputValue}`
+
+    imgQR.addEventListener('load',()=>{
+    container.classList.add('active')
+    qrcodebtn.innerText = 'Código criado!'
+    })
+}
+
+qrcodebtn.addEventListener('click',()=>{
+    gerarQRcode()
+})
+
+QRinput.addEventListener('keydown',(e)=>{
+    if(e.code === 'Enter'){
+        gerarQRcode()
+    }
+})
+
+
+// Limpando código QR
+
+QRinput.addEventListener('keyup',(e)=>{
+    if(!QRinput.value){
+         container.classList.remove('active')
+    qrcodebtn.innerText = ""
+    }
+})
